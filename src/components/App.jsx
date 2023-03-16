3d4176c19eec67578bd884ae40bc1a94cb305552
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-03-16 11:24:56 +0200

    well-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,18 @@ import ContactForm from './ContactForm';
 import ContactFilter from './ContactFilter';
 import ContactList from './ContactList';
 
+const baseContact = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export function App() {
   const [contacts, setContacts] = useState(
-    () =>
-      JSON.parse(localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
+    () => JSON.parse(localStorage.getItem('contacts')) ?? baseContact
   );
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
